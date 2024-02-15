26f8d9683b51946ad1b77dd6351847a58326612e
Molly Sandler <74861421+mollysandler@users.noreply.github.com>
2024-02-15 15:54:56 -0800

    inital setup

--- Driver.java
+++ Driver.java
@@ -1,9 +1,23 @@
+import java.util.*;
+
 public class Driver {
+    DummyFrame frame = new DummyFrame();
+
+    public static void main(String[] args){
+        FileClass one = new FileClass("molly", 1, 1, 1, 1, "Jemma");
+        FileClass two = new FileClass("molly", 0, 1, 1, 1, null);
+        FileClass three = new FileClass(null, 0, 0, 0, 0, null);
+        FileClass four = new FileClass(null, -1, -2, -3, -4, null);
+        FileClass five = new FileClass("molly", -1, -2, -3, -4, "Jemma");
 
 
+        ArrayList<FileClass> data = new ArrayList<>();
+        data.add(one);
+        data.add(two);
+        data.add(three);
+        data.add(four);
+        data.add(five);
 
-
-    public static void main(String args[]){
-        //content
+        System.out.println(data);
     }
 }
---
+++ FileClass.java
@@ -0,0 +1,51 @@
+public class FileClass {
+    private String name;
+    private int linesOfCode;
+    private int local;
+    private int global;
+    private int interfaces;
+    private String parent;
+
+    public FileClass(String name, int linesOfCode, int local, int global, int interfaces, String parent) {
+        this.name = name;
+        this.linesOfCode = linesOfCode;
+        this.local = local;
+        this.global = global;
+        this.interfaces = interfaces;
+        this.parent = parent;
+    }
+
+    public String getName() { return name;}
+
+    public int getLinesOfCode() { return linesOfCode;}
+
+    public int getLocal() {return local;}
+
+    public int getGlobal() {return global;}
+
+    public int getInterfaces() {return interfaces;}
+
+    public String getParent() {return parent;}
+
+    public void setName(String name) {this.name = name;}
+
+    public void setLinesOfCode(int linesOfCode) {
+        this.linesOfCode = linesOfCode;
+    }
+
+    public void setLocal(int local) {
+        this.local = local;
+    }
+
+    public void setGlobal(int global) {
+        this.global = global;
+    }
+
+    public void setInterfaces(int interfaces) {
+        this.interfaces = interfaces;
+    }
+
+    public void setParent(String parent) {
+        this.parent = parent;
+    }
+}
